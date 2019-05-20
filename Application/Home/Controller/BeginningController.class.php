5c990aa6e4d310d3270622027b3fd84b249b862a
arkssss <522500442@qq.com>
2019-05-20 12:27:17 +1000

    520_

--- Application/Home/Controller/BeginningController.class.php
+++ Application/Home/Controller/BeginningController.class.php
@@ -3,7 +3,14 @@ namespace Home\Controller;
 use Think\Controller;
 class BeginningController extends Controller {
     public function index(){
-        $this->display('Beginning/index');  
+        $this->display("Beginning/my_520");
+        // $this->display('Beginning/index');  
+    }
+
+    public function Beginning(){
+
+        $this->display('Beginning/index'); 
+        
     }
 
     
